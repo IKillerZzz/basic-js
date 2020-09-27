0194e63961285c486ff09821c7877eff61d29ef2
Ilya Pol <aliveshooter@gmail.com>
2020-09-27 23:05:59 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) { throw new Error; }
+  let res = [];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (res.length !== 0 && arr[i - 2] !== '--discard-next') { res.pop(); }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) { res.push(arr[i + 1]); }
+        break;
+      case '--double-prev':
+        if (i !== 0 && arr[i - 2] !== '--discard-next') { res.push(arr[i - 1]); }
+        break;
+      default:
+        res.push(arr[i]);
+    }
+  }
+  return res;
+}
